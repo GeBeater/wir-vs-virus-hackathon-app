d46167d38e9c826872b10e34c4c7abb57a9066da
Piers Wermbter <piers@wermbter.com>
2020-03-21 14:00:05 +0100

    ad basic payments

---
+++ src/pages/Payment.js
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from 'react';
+import DropIn from "braintree-web-drop-in-react";
+
+export default function Payment() {
+    const [token, setToken] = useState(null);
+    const [instance, setInstance] = useState(null);
+    useEffect(() => {
+        fetch("/api/payment/token", {method: 'POST'}).then(resp => {
+            return resp.json();
+        }).then(json => {
+            setToken(json.token);
+        });
+
+    }, [])
+
+    async function buy() {
+        // Send the nonce to your server
+        const {nonce} = await instance.requestPaymentMethod();
+        const data = {
+            nonce,
+            amount: 10.00
+        }
+        await fetch('/api/payment/checkout', {
+            method: "POST",
+            headers: {
+                "Content-Type": 'application/json',
+            },
+            body: JSON.stringify(data)
+        });
+    }
+    return (
+        <div>
+            {token &&
+                <>
+                    <DropIn
+                        options={{
+                            authorization: token
+                        }}
+                        onInstance={setInstance}
+                    />
+                    <button onClick={buy}>Buy</button>
+                </>
+            }
+
+            {/* <button onClick={this.buy.bind(this)}>Buy</button> */}
+        </div>
+    )
+}
