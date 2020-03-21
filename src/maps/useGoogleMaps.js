635728990956f312c1f6c0320b2cd94c2fcb14bf
Piers Wermbter <piers@wermbter.com>
2020-03-21 10:44:53 +0100

    remove default UI

--- src/maps/useGoogleMaps.js
+++ src/maps/useGoogleMaps.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import {useEffect, useState, useRef} from "react";
 import GoogleMapsApiLoader from "google-maps-api-loader";
 
 const apiKey = process.env.REACT_APP_GOOGLE_KEY;
@@ -8,20 +8,20 @@ const eventsMapping = {
   onBoundsChangerd: ["bounds_changed", map => map.getBounds()]
 };
 
-export default function useGoogleMap({ zoom, center, events }) {
-  const [mapState, setMapState] = useState({ loading: true });
+export default function useGoogleMap({zoom, center, events}) {
+  const [mapState, setMapState] = useState({loading: true});
   const mapRef = useRef();
   useEffect(() => {
-    GoogleMapsApiLoader({ apiKey }).then(google => {
-      const map = new google.maps.Map(mapRef.current, { zoom, center });
+    GoogleMapsApiLoader({apiKey}).then(google => {
+      const map = new google.maps.Map(mapRef.current, {zoom, center, disableDefaultUI: true});
       Object.keys(events).forEach(eventName =>
         map.addListener(eventsMapping[eventName][0], () =>
           events[eventName](eventsMapping[eventName][1](map))
         )
       );
 
-      setMapState({ maps: google.maps, map, loading: false });
+      setMapState({maps: google.maps, map, loading: false});
     });
   }, []);
-  return { mapRef, ...mapState };
+  return {mapRef, ...mapState};
 }
