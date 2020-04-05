7b604e6afacddef143fc4e4c5eb90fe1d55d2c27
Paul Schkade <mail@paulschkade.net>
2020-04-05 15:03:29 +0200

    mobile navigation

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -10,6 +10,12 @@ import Help from "../assets/help-icon.svg";
 import {Link} from "react-router-dom";
 import {Imprint} from "../pages/Imprint";
 import {Privacy} from "../pages/Privacy";
+import MenuIcon from '@material-ui/icons/Menu';
+import Hidden from "@material-ui/core/Hidden";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 const useStyles = makeStyles(theme => ({
     appBar: {
@@ -23,9 +29,11 @@ const useStyles = makeStyles(theme => ({
         display: 'flex'
     },
     button: {
-        marginLeft: spacing.m,
         padding: '10px 15px',
-        width: '100%'
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: spacing.m,
+        }
     },
     buttonIcon: {
         marginRight: '8px'
@@ -35,8 +43,13 @@ const useStyles = makeStyles(theme => ({
         fontFamily: 'Montserrat',
         fontWeight: '600'
     },
+    mobileMenuIcon: {
+        color: colors.black
+    },
+    drawerPaper: {
+        width: 'calc(100% - 70px)'
+    }
 }));
-
 
 export const HeaderBarItem = ({text, icon, handleClick}) => {
     const classes = useStyles();
@@ -53,9 +66,20 @@ export const HeaderBar = () => {
     const [faqOpen, setFaqOpen] = React.useState(false);
     const [imprintOpen, setImprintOpen] = React.useState(false);
     const [privacyOpen, setPrivacyOpen] = React.useState(false);
-    const toggleFaqOpen = () => setFaqOpen(!faqOpen);
-    const toggleImprintOpen = () => setImprintOpen(!imprintOpen);
-    const togglePrivacyOpen = () => setPrivacyOpen(!privacyOpen);
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+    const toggleFaqOpen = () => {
+        setMobileOpen(false);
+        setFaqOpen(!faqOpen);
+    };
+    const toggleImprintOpen = () => {
+        setMobileOpen(false);
+        setImprintOpen(!imprintOpen);
+    };
+    const togglePrivacyOpen = () => {
+        setMobileOpen(false);
+        setPrivacyOpen(!privacyOpen);
+    };
+    const toggleMobileOpen = () => setMobileOpen(!mobileOpen);
 
     return (
         <AppBar position="static" className={classes.appBar}>
@@ -66,11 +90,43 @@ export const HeaderBar = () => {
                     </Link>
                 </div>
                 <div className={classes.buttonGroup}>
-                    <HeaderBarItem text={'FAQ'} icon={Help} handleClick={toggleFaqOpen} />
-                    <HeaderBarItem text={'Impressum'} handleClick={toggleImprintOpen} />
-                    <HeaderBarItem text={'Datenschutz'} handleClick={togglePrivacyOpen} />
+                    {/* mobile burger icon */}
+                    <Hidden smUp implementation="js">
+                        <MenuIcon onClick={toggleMobileOpen} className={classes.mobileMenuIcon} />
+                    </Hidden>
+                    {/* desktop nav items */}
+                    <Hidden xsDown implementation="js">
+                        <HeaderBarItem text={'FAQ'} icon={Help} handleClick={toggleFaqOpen} />
+                        <HeaderBarItem text={'Impressum'} handleClick={toggleImprintOpen} />
+                        <HeaderBarItem text={'Datenschutz'} handleClick={togglePrivacyOpen} />
+                    </Hidden>
                 </div>
             </Toolbar>
+            {/* mobile navigation */}
+            <Drawer
+                variant="temporary"
+                anchor={'right'}
+                open={mobileOpen}
+                onClose={toggleMobileOpen}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+                ModalProps={{
+                    keepMounted: true, // Better open performance on mobile.
+                }}
+            >
+                <List>
+                    <ListItem key={'faq'} onClick={toggleFaqOpen}>
+                        <ListItemText primary={'FAQ'} />
+                    </ListItem>
+                    <ListItem key={'Impressum'} onClick={toggleImprintOpen}>
+                        <ListItemText primary={'Impressum'} />
+                    </ListItem>
+                    <ListItem key={'Datenschutz'} onClick={togglePrivacyOpen}>
+                        <ListItemText primary={'Datenschutz'} />
+                    </ListItem>
+                </List>
+            </Drawer>
             <FAQ open={faqOpen} handleClose={toggleFaqOpen}/>
             <Imprint open={imprintOpen} handleClose={toggleImprintOpen}/>
             <Privacy open={privacyOpen} handleClose={togglePrivacyOpen} />
