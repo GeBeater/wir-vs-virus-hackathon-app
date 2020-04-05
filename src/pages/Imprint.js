c50799984ff6fc5e869e5804b115972286723088
Paul Schkade <mail@paulschkade.net>
2020-04-05 20:20:59 +0200

    add imprint and privacy content

--- src/pages/Imprint.js
+++ src/pages/Imprint.js
@@ -8,32 +8,44 @@ import {useTranslation} from 'react-i18next';
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles(theme => ({
-    container: {
-        width: 80,
-        display: 'flex',
-        height: '100%',
-        flexDirection: 'column'
+    imprintContent: {
+        fontSize: '0.75em',
+        lineHeight: 1.5
     },
-    faqItem: {
-        marginBottom: theme.spacing(2)
-    }
 }));
 
 export const Imprint = ({open, handleClose}) => {
     const {t} = useTranslation();
+    const classes = useStyles();
 
     return (
         <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open} maxWidth={'md'}>
             <DialogContent dividers>
                 <b>Impressum</b>
             </DialogContent>
-            <DialogContent dividers>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Habent enim et bene longam et satis litigiosam disputationem. A villa enim, credo, et: Si ibi te esse scissem, ad te ipse venissem. Sed hoc sane concedamus. <a href="http://loripsum.net/" target="_blank">Peccata paria.</a> Atque hoc loco similitudines eas, quibus illi uti solent, dissimillimas proferebas. <i>Duo Reges: constructio interrete.</i> Amicitiam autem adhibendam esse censent, quia sit ex eo genere, quae prosunt. </p>
+            <DialogContent dividers className={classes.imprintContent}>
+                <h2>Angaben gemäß § 5 TMG</h2>
+                <p>Social Impact gGmbH<br />
+                    Schiffbauergasse 7<br />
+                    14467 Potsdam</p>
 
-                <p>Tu vero, inquam, ducas licet, si sequetur; <mark>Primum Theophrasti, Strato, physicum se voluit;</mark> <mark>Hoc sic expositum dissimile est superiori.</mark> Tu enim ista lenius, hic Stoicorum more nos vexat. <i>Si enim ad populum me vocas, eum.</i> Hoc etsi multimodis reprehendi potest, tamen accipio, quod dant. <a href="http://loripsum.net/" target="_blank">Perge porro;</a> <a href="http://loripsum.net/" target="_blank">Idem iste, inquam, de voluptate quid sentit?</a> </p>
+                <p>Handelsregister: HRB 19 157 P<br />
+                    Registergericht: Amtsgericht Potsdam</p>
 
-                <p>Qui autem esse poteris, nisi te amor ipse ceperit? <b>Si qua in iis corrigere voluit, deteriora fecit.</b> Cuius etiam illi hortuli propinqui non memoriam solum mihi afferunt, sed ipsum videntur in conspectu meo ponere. <a href="http://loripsum.net/" target="_blank">Quippe: habes enim a rhetoribus;</a> </p>
+                <p><strong>Vertreten durch:</strong><br />
+                    Norbert Kunz, Gabriela Spangenberg, Jörg Fürstenberger</p>
 
+                <h2>Kontakt</h2>
+                <p>Telefon: +491787261209<br />
+                    E-Mail: info@cofund.de</p>
+
+                <h2>Umsatzsteuer-ID</h2>
+                <p>Umsatzsteuer-Identifikationsnummer gemäß § 27 a Umsatzsteuergesetz:<br />
+                    DE232834691</p>
+
+                <h2>Verbraucher­streit­beilegung/Universal­schlichtungs­stelle</h2><p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+
+                <h3>Haftung für Inhalte</h3> <p>Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.</p> <p>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p> <h3>Haftung für Links</h3> <p>Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.</p> <p>Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p> <h3>Urheberrecht</h3> <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.</p> <p>Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
             </DialogContent>
             <DialogActions>
                 <Button autoFocus onClick={handleClose} color="primary">
